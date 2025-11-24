d9e8e1a6279cb64935d6cf8e375bb5927fba3011
OmriRitblat <ritblat@post.bgu.il>
2025-11-24 20:46:34 +0200

    until q5

--- nearest_neighbour.py
+++ nearest_neighbour.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
 
 
 def gensmallm(x_list: list, y_list: list, m: int):
@@ -28,10 +29,17 @@ def gensmallm(x_list: list, y_list: list, m: int):
 # todo: complete the following functions, you may add auxiliary functions or define class to help you
 
 def learnknn(k: int, x_train: np.array, y_train: np.array):
-    dict={}
-    for i in range(len(x_train)):
-        dict[tuple(x_train[i])] = y_train[i]
-    return dict
+    def knn_predict(x_test: np.array):
+        preds = []
+        for i in range(len(x_test)):
+            distances = np.linalg.norm(x_train - x_test[i], axis=1)
+            k_indices = np.argsort(distances)[:k]
+            k_nearest_labels = y_train[k_indices]
+            values, counts = np.unique(k_nearest_labels, return_counts=True)
+            predicted_label = values[np.argmax(counts)]
+            preds.append(predicted_label)
+        return np.array(preds).reshape(-1, 1)
+    return knn_predict
 def predictknn(classifier, x_test: np.array):
     """
 
@@ -39,16 +47,8 @@ def predictknn(classifier, x_test: np.array):
     :param x_test: numpy array of size (n, d) containing test examples that will be classified
     :return: numpy array of size (n, 1) classifying the examples in x_test
     """
-    preds = []
-    x_train = np.array(list(classifier.keys()))
-    y_train = np.array(list(classifier.values()))
-    for i in range(len(x_test)):
-        distances = np.linalg.norm(x_train - x_test[i], axis=1)
-        k_indices = np.argsort(distances)[:5]
-        k_nearest_labels = y_train[k_indices]
-        values, counts = np.unique(k_nearest_labels, return_counts=True)
-        predicted_label = values[np.argmax(counts)]
-        preds.append(predicted_label)
+
+    preds = classifier(x_test)
     return np.array(preds).reshape(-1, 1)
 
 def test_knn():
@@ -60,7 +60,7 @@ def test_knn():
     x_test = np.array([[0.9, 0.9], [0.1, 0.2], [0.2, 0.8]])
     
     # Train and predict
-    classifier = learnknn(3, x_train, y_train)
+    classifier = learnknn(1, x_train, y_train)
     preds = predictknn(classifier, x_test)
     
     print("Predictions:", preds.flatten())
@@ -99,11 +99,134 @@ def simple_test():
     # this line should print the classification of the i'th test sample.
     print(f"The {i}'th test sample was classified as {preds[i]}")
 
+def knn_experiment(sample_sizes, repeats=10):
+    avg_errors = []
+    min_errors = []
+    max_errors = []
 
+    data = np.load('mnist_all.npz')
+    train0 = data['train0']
+    train1 = data['train1']
+    train2 = data['train2']
+    train3 = data['train3']
+
+    test0 = data['test0']
+    test1 = data['test1']
+    test2 = data['test2']
+    test3 = data['test3']
+
+    x_train_full, y_train_full = gensmallm([train0, train1, train2, train3], [0, 1, 2, 3], 100)
+
+    x_test, y_test = gensmallm([test0, test1, test2, test3], [0, 1, 2, 3], 50)
+    for m in sample_sizes:
+        errors = []
+        for _ in range(repeats):
+            x_train, y_train = gensmallm([train0, train1, train2, train3], [0, 1, 2, 3], m)
+            classifier = learnknn(1, x_train, y_train)
+            y_pred=predictknn(classifier, x_test).flatten()
+            error = np.mean(y_test != y_pred)
+            errors.append(error)
+        avg_errors.append(np.mean(errors))
+        min_errors.append(np.min(errors))
+        max_errors.append(np.max(errors))
+
+    plt.errorbar(sample_sizes, avg_errors, yerr=[np.array(avg_errors)-np.array(min_errors), np.array(max_errors)-np.array(avg_errors)], fmt='-o')
+    plt.xlabel('Training Sample Size')
+    plt.ylabel('Average Test Error')
+    plt.title('k-NN (k=1) Test Error vs Training Size')
+    plt.ylim(0, 1)
+    plt.show()
+
+def knn_experiment_30_runs(sample_sizes, repeats=30):
+    data = np.load('mnist_all.npz')
+    train0 = data['train0']
+    train1 = data['train1']
+    train2 = data['train2']
+    train3 = data['train3']
+    test0 = data['test0']
+    test1 = data['test1']
+    test2 = data['test2']
+    test3 = data['test3']
+    x_test, y_test = gensmallm([test0, test1, test2, test3], [0, 1, 2, 3], 200)
+
+    for m in sample_sizes:
+        avg_errors = []
+        for k in range(1, 16):
+            errors = []
+            for _ in range(repeats):
+                x_train, y_train = gensmallm([train0, train1, train2, train3], [0, 1, 2, 3], m)
+                classifier = learnknn(k, x_train, y_train)
+                y_pred = predictknn(classifier, x_test).flatten()
+                error = np.mean(y_test != y_pred)
+                errors.append(error)
+            avg_errors.append(np.mean(errors))
+        plt.figure()
+        plt.plot(range(1, 16), avg_errors, marker='o')
+        plt.xlabel('k')
+        plt.ylabel('Average Test Error')
+        plt.title(f'k-NN Test Error vs k (Training Size={m})')
+        plt.ylim(0, 1)
+        plt.grid(True)
+        plt.show()
+        optimal_k = 1 + avg_errors.index(min(avg_errors))
+        print(f"Optimal k for m={m}: {optimal_k} (error={min(avg_errors):.3f})")
+
+
+def knn_experiment_30_runs_corrupted_labels(sample_sizes, repeats=30):
+    data = np.load('mnist_all.npz')
+    train0 = data['train0']
+    train1 = data['train1']
+    train2 = data['train2']
+    train3 = data['train3']
+    test0 = data['test0']
+    test1 = data['test1']
+    test2 = data['test2']
+    test3 = data['test3']
+    x_test_orig, y_test_orig = gensmallm([test0, test1, test2, test3], [0, 1, 2, 3], 200)
+
+    def corrupt_labels(y, num_classes=4, corruption_ratio=0.3):
+        y_corrupt = y.copy()
+        n = len(y_corrupt)
+        num_corrupt = int(corruption_ratio * n)
+        corrupt_indices = np.random.choice(n, num_corrupt, replace=False)
+        for idx in corrupt_indices:
+            current_label = y_corrupt[idx]
+            other_labels = [l for l in range(num_classes) if l != current_label]
+            y_corrupt[idx] = np.random.choice(other_labels)
+        return y_corrupt
+
+    for m in sample_sizes:
+        avg_errors = []
+        for k in range(1, 16):
+            errors = []
+            for _ in range(repeats):
+                x_train, y_train = gensmallm([train0, train1, train2, train3], [0, 1, 2, 3], m)
+                y_train_corrupt = corrupt_labels(y_train)
+                y_test_corrupt = corrupt_labels(y_test_orig)
+                classifier = learnknn(k, x_train, y_train_corrupt)
+                y_pred = predictknn(classifier, x_test_orig).flatten()
+                error = np.mean(y_test_corrupt != y_pred)
+                errors.append(error)
+            avg_errors.append(np.mean(errors))
+        plt.figure()
+        plt.plot(range(1, 16), avg_errors, marker='o')
+        plt.xlabel('k')
+        plt.ylabel('Average Test Error')
+        plt.title(f'k-NN Test Error vs k (Training Size={m}, 30% labels corrupted)')
+        plt.ylim(0, 1)
+        plt.grid(True)
+        plt.show()
+        optimal_k = 1 + avg_errors.index(min(avg_errors))
+        print(f"Optimal k for m={m} (corrupted labels): {optimal_k} (error={min(avg_errors):.3f})")
 if __name__ == '__main__':
 
     # before submitting, make sure that the function simple_test runs without errors
-    # simple_test()
-    test_knn()
+    #simple_test()
+    #test_knn()
+
+    
+    #knn_experiment(sample_sizes=[10, 20, 35, 50, 75, 100])
+    #knn_experiment_30_runs(sample_sizes=[50, 150, 500], repeats=30)
+    knn_experiment_30_runs_corrupted_labels(sample_sizes=[50, 150, 500], repeats=30)
 
 
